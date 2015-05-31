6cbc194b76eaff67728445f47c120e9ea8b94eef
superfoght <bo@redweb.dk>
2015-05-31 11:45:44 +0200

    Sidebars and main content view
    Added sidebars and main content view to grid layout

--- index.php
+++ index.php
@@ -49,6 +49,37 @@ defined('_JEXEC') or die('Restricted access');
 
 </div>
 
+<!-- Joomla Content -->
+<div class="container">
+    <?php if ($this->countModules( 'sidebar1 or sidebar2' )) : ?>
+        <?php if ($this->countModules( 'sidebar1' )) : ?>
+            <div id="sidebar1" class="col-sm-3">
+                <jdoc:include type="modules" name="sidebar1" style="xhtml" />
+            </div>
+        <?php endif; ?>
+        <?php if ($this->countModules( 'sidebar1 and sidebar2' )) : ?>
+            <div id="main" class="col-sm-6">
+                <jdoc:include type="component" />
+            </div>
+        <?php else : ?>
+
+            <div id="main" class="col-sm-9">
+                <jdoc:include type="component" />
+            </div>
+        <?php endif; ?>
+
+        <?php if ($this->countModules( 'sidebar2' )) : ?>
+            <div id="sidebar2" class="col-sm-3">
+                <jdoc:include type="modules" name="sidebar2" style="xhtml" />
+            </div>
+        <?php endif; ?>
+
+    <?php else : ?>
+        <div class="col-sm-12">
+            <jdoc:include type="component" />
+        </div>
+    <?php endif; ?>
+</div>
 
 
 <!-- Bootstrap core JavaScript
